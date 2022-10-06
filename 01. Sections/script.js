86e94f776bf5f29e559b4bb92146fb9bf95e2208
kr0k094 <genadizhelyazkov94@gmail.com>
2022-10-06 10:32:01 +0200

    adding dom

--- 01. Sections/script.js
+++ 01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+  let content = document.getElementById("content");
+
+
+  for (const word of words) {
+     let p = document.createElement("p");
+     let div = document.createElement("div");
+     p.textContent = word;
+     p.style.display = "none";
+     div.appendChild(p);
+     div.addEventListener("click", onClick);
+     content.appendChild(div);
+  }
+  function onClick(e) {
+     e.currentTarget.children[0].style.display = ""
+  }
 }
